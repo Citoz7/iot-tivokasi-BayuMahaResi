653b9ebb71f5f6b70567b7ddfa971b7a0f917205
Bayu Maha Resi <bayumaharesi@gmail.com>
2025-05-09 21:59:55 +0700

    Tambah folder dan file praktikum baru di branch master

---
+++ PRAKTIKUM IoT_1/src/main.cpp
@@ -0,0 +1,29 @@
+#include <Arduino.h>
+
+int lampu1 =26;
+int lampu2 =25;
+int lampu3 =27;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(lampu1, OUTPUT);
+  pinMode(lampu2, OUTPUT);
+  pinMode(lampu3, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(lampu1, HIGH);
+  digitalWrite(lampu1, LOW);
+  delay(2000); 
+
+  digitalWrite(lampu2, HIGH);
+  digitalWrite(lampu2, LOW);
+  delay(2000); 
+ 
+  digitalWrite(lampu3, HIGH);
+  digitalWrite(lampu3, LOW);
+  delay(2000); 
+  
+
+}
